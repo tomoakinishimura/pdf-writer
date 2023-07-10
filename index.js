bd861e92276ac1a502815e71c2563b8ff77eb283
tomoanis <tomoanis@yahoo-corp.jp>
2023-07-11 08:50:06 +0900

    フォントサイズを考慮、PDFのサイズを考慮

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
 const fs = require('fs');
 const path = require('path');
-const hummus = require('hummus');
+const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
 
 const inputFolderPath = './data'; // フォルダのパスを指定
 const outputFolderPath = './output'; // 出力先フォルダのパスを指定
 
 // フォルダ内のファイルを再帰的に処理する関数
-function processFilesInFolder(folderPath) {
+async function processFilesInFolder(folderPath) {
     const files = fs.readdirSync(folderPath);
 
-    files.forEach((file) => {
+    for (const file of files) {
         const filePath = path.join(folderPath, file);
         const fileStats = fs.statSync(filePath);
 
         if (fileStats.isDirectory()) {
             // サブフォルダが見つかった場合、再帰的に処理
-            processFilesInFolder(filePath);
+            await processFilesInFolder(filePath);
         } else if (file.endsWith('.pdf')) {
             // PDFファイルの場合、編集処理を行う
             const outputFilePath = path.join(outputFolderPath, file);
 
-            const pdfWriter = hummus.createWriterToModify(filePath, {
-                modifiedFilePath: outputFilePath,
-            });
+            const pdfBytes = fs.readFileSync(filePath);
 
-            // フォントファイルをインポート
-            const font = pdfWriter.getFontForFile("/System/Library/Fonts/NewYork.ttf");
+            // PDFDocumentオブジェクトを作成
+            const pdfDoc = await PDFDocument.load(pdfBytes);
+            const font = await pdfDoc.embedFont(StandardFonts.TimesRoman)
 
-            const pdfParser = pdfWriter.getModifiedFileParser();
-            const pageTotalCount = pdfParser.getPagesCount();
-            for (let pageIndex = 0; pageIndex < pageTotalCount; pageIndex++) {
-                const pageModifier = new hummus.PDFPageModifier(pdfWriter, pageIndex);
+            const pageCount = pdfDoc.getPageCount();
+            for (let pageIndex = 0; pageIndex < pageCount; pageIndex++) {
+                const page = pdfDoc.getPage(pageIndex);
+                // ページの幅と高さを取得
+                const pageWidth = page.getWidth();
+                const pageHeight = page.getHeight();
+
+                const fontSize = 12;
                 const fileName = file.split('_')[0]; // ファイル名から最初の数字を取得
+                const textWidth = font.widthOfTextAtSize(fileName, fontSize)
+                const textHeigth = font.heightAtSize(fontSize);
 
-                // const x = 100; // x座標: 左からの距離
-                // const y = 100; // y座標: 上からの距離
-
-                pageModifier.startContext().getContext().writeText(
-                    fileName, // 入力文字列
-                    500,800,
-                    // pageWidth - x, // 右からの距離（右端から左方向に負の値）
-                    // pageHeight - y, // 上からの距離（上端から下方向に正の値）
-                    {
-                        font: font,         // フォントの指定
-                        size: 12,           // 文字サイズの指定
-                        colorspace: 'gray', // 色空間を"gray", "cmyk", "rgb"から選択
-                        color: 0x00         // カラーコード
-                    }
-                );
-                pageModifier.endContext().writePage();
+                // テキストの基準点を右上隅として指定
+                page.drawText(fileName, {
+                    x: pageWidth - textWidth - 5, // x座標: 左からの距離
+                    y: pageHeight - textHeigth - 5, // y座標: 上からの距離
+                    size: fontSize, // テキストのサイズ
+                    font: font,
+                    color: rgb(0, 0, 0), // テキストの色 (黒色)
+                });
             }
 
-            pdfWriter.end();
+            // 編集したPDFを保存
+            const modifiedPdfBytes = await pdfDoc.save();
+            fs.writeFileSync(outputFilePath, modifiedPdfBytes);
         }
-    });
+    }
 }
 
 // 出力先フォルダを作成
 fs.mkdirSync(outputFolderPath, { recursive: true });
 
 // フォルダ内のファイルを処理
-processFilesInFolder(inputFolderPath);
+processFilesInFolder(inputFolderPath).catch((error) => {
+    console.error('エラー:', error);
+});
