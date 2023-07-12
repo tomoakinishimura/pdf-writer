505293ebc98fc925902001bf5212e42c42bd1890
tomoanis <tomoanis@yahoo-corp.jp>
2023-07-12 09:26:40 +0900

    一旦完成

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # pdf-writer
-pdf writer
 
-/dataに入れたフォルダを再起的に確認
-ファイル名の_によってpdfに入れるべき値を取得
+指定フォルダを再起的にPDFファイルの書き換えを行います
+Windowsコマンドプロンプトにてダウンロードした指定execファイルを実行してください
+```shell
+C> ./pdw-writer.exe --input フォルダ --output フォルダ
+```
+他下記方法のオプションが利用できます
 
-/outputに出力
+```shell
+Usage: ./pdf-writer [options]
 
-# サンプル
+Options:
+--input <inputFolderPath>    Specify the input folder path.
+--output <outputFolderPath>  Specify the output folder path.
+--size <fontSize>            Specify the font size. (default: 12)
+--color <R,G,B>              Specify the text color in RGB format. (default: 0,0,0)
+--x <xCoordinate>            Specify the x-coordinate. (default: 5)
+--y <yCoordinate>            Specify the y-coordinate. (default: 5)
+--help                       Display this help message.
+```
 
-- インプット
-![12345_hogehoge](https://github.com/tomoakinishimura/pdf-writer/assets/17454725/6d46baea-0901-4227-b93c-1988a86bc826)
+## 開発向け
 
+```shell
+# for mac
+deno compile --unstable --allow-read --allow-write --no-check  pdf-writer.ts
 
-- アウトプット
-![12345_hogehoge](https://github.com/tomoakinishimura/pdf-writer/assets/17454725/318bbd26-667e-4670-9155-ea2278475856)
+# for windows
+deno compile --unstable --allow-read --allow-write --no-check --target x86_64-pc-windows-msvc  pdf-writer.ts
+```
---
+++ pdf-writer.ts
@@ -0,0 +1,88 @@
+import { ensureDir } from 'https://deno.land/std/fs/mod.ts';
+import { join, extname } from 'https://deno.land/std/path/mod.ts';
+import { PDFDocument, StandardFonts, rgb } from 'https://cdn.pika.dev/pdf-lib@^1.7.0';
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const parsedArgs = parse(Deno.args);
+const inputFolderPath = parsedArgs.input;
+const outputFolderPath = parsedArgs.output;
+const fontSize = parsedArgs.size ?? 12;
+const colorRGB = parsedArgs.color ? parsedArgs.color.split(',').map(Number).map(val => val / 255) : [0, 0, 0];
+const x = parsedArgs.x ?? 5;
+const y = parsedArgs.y ?? 5;
+
+if (parsedArgs.help) {
+    console.log(`Usage: ./pdf-writer [options]
+
+Options:
+  --input <inputFolderPath>    Specify the input folder path.
+  --output <outputFolderPath>  Specify the output folder path.
+  --size <fontSize>            Specify the font size. (default: 12)
+  --color <R,G,B>              Specify the text color in RGB format. (default: 0,0,0)
+  --x <xCoordinate>            Specify the x-coordinate. (default: 5)
+  --y <yCoordinate>            Specify the y-coordinate. (default: 5)
+  --help                       Display this help message.
+`);
+    Deno.exit(0);
+}
+
+if (!inputFolderPath || !outputFolderPath) {
+    console.error('Error: Input folder path and output folder path must be specified. Use --help for more information.');
+    Deno.exit(1);
+}
+
+// フォルダ内のファイルを再帰的に処理する関数
+async function processFilesInFolder(inputFolderPath: string, outputFolderPath: string) {
+    const files = await Deno.readDir(inputFolderPath);
+
+    for await (const file of files) {
+        const filePath = join(inputFolderPath, file.name);
+        const fileStats = await Deno.stat(filePath);
+
+        if (fileStats.isDirectory) {
+            // サブフォルダが見つかった場合、再帰的に処理
+            await processFilesInFolder(filePath, outputFolderPath);
+        } else if (file.name.endsWith('.pdf')) {
+            // PDFファイルの場合、編集処理を行う
+            const outputFilePath = join(outputFolderPath, file.name);
+
+            const pdfBytes = await Deno.readFile(filePath);
+
+            // PDFDocumentオブジェクトを作成
+            const pdfDoc = await PDFDocument.load(pdfBytes);
+            const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+
+            const pageCount = pdfDoc.getPageCount();
+            for (let pageIndex = 0; pageIndex < pageCount; pageIndex++) {
+                const page = pdfDoc.getPage(pageIndex);
+                // ページの幅と高さを取得
+                const { width: pageWidth, height: pageHeight } = page.getSize();
+
+                const fileName = file.name.split('_')[0]; // ファイル名から最初の数字を取得
+                const textWidth = font.widthOfTextAtSize(fileName, fontSize);
+                const textHeight = font.heightAtSize(fontSize);
+
+                // テキストの基準点を右上隅として指定
+                page.drawText(fileName, {
+                    x: pageWidth - textWidth - x, // x座標: 左からの距離
+                    y: pageHeight - textHeight - y, // y座標: 上からの距離
+                    size: fontSize, // テキストのサイズ
+                    font,
+                    color: rgb(colorRGB[0], colorRGB[1], colorRGB[2]), // テキストの色 (RGBフォーマット)
+                });
+            }
+
+            // 編集したPDFを保存
+            const modifiedPdfBytes = await pdfDoc.save();
+            await Deno.writeFile(outputFilePath, modifiedPdfBytes);
+        }
+    }
+}
+
+// 出力先フォルダを作成
+await ensureDir(outputFolderPath);
+
+// フォルダ内のファイルを処理
+await processFilesInFolder(inputFolderPath, outputFolderPath).catch((error) => {
+    console.error('Error:', error);
+});
